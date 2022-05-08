b2231256e8676353d43a9ae7c9685b7cd4ef1708
James-Woodland <james.woodland@students.plymouth.ac.uk>
2022-05-08 23:19:44 +0100

    Added comments to primary scripts and AI training

--- Final Product/Host Collector/HostMonitor.py
+++ Final Product/Host Collector/HostMonitor.py
@@ -8,24 +8,28 @@ import joblib
 from sklearn import *
 import json
 
-def getCPUuse():
+
+"""
+Collects CPU usage stats
+"""
+def getCPUStats():
+    vcgm = Vcgencmd()
+    temp = vcgm.measure_temp()
+    volts = vcgm.measure_volts("core")
     freeCpu = str(os.popen("top -b -n1 | awk '/Cpu\(s\):/ {print $8}'").readline().strip(\
     ))
-    return(str(100-float(freeCpu)))
+    return(str(100-float(freeCpu)), temp, volts)
 
-def getTaskNum():
+"""
+Collects statistics related to how many tasks are running
+"""
+def getTaskStats():
     Tasks = str(os.popen("top -b -n1 | awk '/Tasks\:/ {print $0}'").readline().strip(\
 ))
     Tasks = Tasks.split(" ")
     return(Tasks)
 
-def getHostData(conn, cur, Model, Scaler):
-    vcgm = Vcgencmd()
-    temp = vcgm.measure_temp()
-    volts = vcgm.measure_volts("core")
-    cpu = getCPUuse()
-    tasks = getTaskNum()
-
+def getRAMStats():
     p = os.popen('free -t')
     i = 0
     while 1:
@@ -35,54 +39,73 @@ def getHostData(conn, cur, Model, Scaler):
             totalMemUsage = line.split()[2]
             totalMem = line.split()[1]
             break
+    return(totalMemUsage, totalMem)
+    
+
+"""
+Calls the functions to collect host statistics
+Scales the host data and then predicts whether the data indicates malicious activity or not
+Stores the host data and the associated label into the database
+"""
+def getHostData(conn, cur, Model, Scaler):    
+    cpu, temp, volts = getCPUStats()
+    tasks = getTaskStats()
+    totalMemUsage, totalMem = getRAMStats()
+    
     data = [[totalMemUsage, temp, tasks[1], volts, cpu]]
     data = Scaler.transform(data)
     label = Model.predict(data)
     cur.execute("INSERT INTO HOSTDATA (TimeStamp, totalRam, usedRam, cpuPercent, cpuTemp, cpuVolts, totalTasks, runningTasks, sleepingTasks, stoppedTasks, ZombieTasks, mac, label) VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)", (datetime.now(timezone.utc), totalMem, totalMemUsage, cpu, temp, volts, tasks[1], tasks[5],tasks[7],tasks[11],tasks[15], gma(), int(label[0])))
     conn.commit()
-path = os.path.realpath(__file__)
-path  = path.split("/")
-path = "/".join(path[0:-1])
-f = open(path+"/Config.json")
 
-setup = json.load(f)
+"""
+Loads in the config file
+Set up the connection to the database
+Loads in the scaler and Model
+starts a loops so that host data collection will over every 5 seconds
+"""
+if __name__ == "__main__":
+    path = os.path.realpath(__file__)
+    path  = path.split("/")
+    path = "/".join(path[0:-1])
+    f = open(path+"/Config.json")
 
-databaseIP = setup["Database"]["databaseIP"]
-print(f)
-postgresPassword = setup["Database"]["postgresPassword"]
-try:        
-    conn = psycopg2.connect(
-        host=databaseIP,
-        database="pulse",
-        user="postgres",
-        password=postgresPassword)
-    print("connection successful")
-        #print("Connection Successfull")
-except Exception as e:
-    print(e)        
-cur = conn.cursor()
-#l = task.LoopingCall(getHostData)
-#l.start(5.0)
-#reactor.run()
-path = os.path.realpath(__file__)
-path = path.split("/")
-path = path[:-1]
-path = "/".join(path)
-Model = joblib.load(path+"/StackingHost.pkl")
-Scaler = joblib.load(path+"/HostScaler.pkl")
-try:
-    while True:
-        try:
-            start_time = time.time()
-            getHostData(conn, cur, Model, Scaler)
-            time.sleep(5-(time.time()-start_time))
-        except:
-            pass
-except Exception as e:
-    print(e)
-    cur.close()
-    conn.close()
-    print("program finished")
+    setup = json.load(f)
+
+    databaseIP = setup["Database"]["databaseIP"]
+    print(f)
+    postgresPassword = setup["Database"]["postgresPassword"]
+    try:        
+        conn = psycopg2.connect(
+            host=databaseIP,
+            database="pulse",
+            user="postgres",
+            password=postgresPassword)
+        print("connection successful")
+            
+    except Exception as e:
+        print(e)        
+    cur = conn.cursor()
+    
+    path = os.path.realpath(__file__)
+    path = path.split("/")
+    path = path[:-1]
+    path = "/".join(path)
+    Model = joblib.load(path+"/StackingHost.pkl")
+    Scaler = joblib.load(path+"/HostScaler.pkl")
+    try:
+        while True:
+            try:
+                start_time = time.time()
+                getHostData(conn, cur, Model, Scaler)
+                time.sleep(5-(time.time()-start_time))
+            except:
+                pass
+    except Exception as e:
+        print(e)
+        cur.close()
+        conn.close()
+        print("program crashed")
 
 
 
--- Final Product/Network Collector/PiCollection.py
+++ Final Product/Network Collector/PiCollection.py
@@ -11,25 +11,22 @@ import joblib
 import sklearn
 from grafana_api.grafana_face import GrafanaFace
 import json
-    
-def test(pkt):
+
+"""
+Function for processing packets once they've been captured
+Check what the communication types is, extracts the needed features and predicts whther the oacket is malicious or not
+It then stores the packet info and its label in the database. 
+"""
+def packetProcessing(pkt):
     try:
-        if (pkt[IP].proto == 17 or pkt[IP].proto == 6):
-            #IP1 = pkt[IP].src.split(".")
-            #IP2 = pkt[IP].dst.split(".")
-            #features = [[pkt.sport, pkt.dport, int(IP1[0]), int(IP1[1]), int(IP1[2]), int(IP1[3]), int(IP2[0]), int(IP2[1]), int(IP2[2]), int(IP2[3])]]
+        if (pkt[IP].proto == 17 or pkt[IP].proto == 6):            
             if "192.168" in pkt[IP].src and "192.168" in pkt[IP].dst:
                 internal = 1
             else:
                 internal = 0
             features = [[pkt.sport, pkt.dport, internal]]
             features = Scaler.transform(features)
-            label = Model.predict(features)
-            #print([pkt.sport, pkt.dport])
-            #print(label)
-            #print(label[0])
-            #print("*********************************************************************")
-            #print(ls(pkt))
+            label = Model.predict(features)            
             cursor = conn.cursor()
             cursor.execute("INSERT INTO NetworkData (TimeStamp, SMac, DMac, SIp, DIp, sport, dport,TTL, TOS, ID, IHL, DLen, Proto, label) VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)", (datetime.now(timezone.utc), pkt.src, pkt.dst, pkt[IP].src
                                                                                                                                                                                               , pkt[IP].dst, pkt.sport, pkt.dport ,
@@ -39,9 +36,15 @@ def test(pkt):
             cursor.close()
             
     except Exception as e:
-        #ls(pkt)
         print(e)
         pass
+
+"""
+Performs the initial device scan
+If a fullscan is requested every ip in the range is checked
+If a fullscan is not needed and IPs are supplied it will only scan for the given IPs
+The MAC address, IP Address and the vendor the MAC belongs to is recorded and returned 
+"""
         
 def deviceScan(fullScan):      
     nmap_path = [r"C:\Program Files (x86)\Nmap\nmap.exe"]
@@ -49,47 +52,40 @@ def deviceScan(fullScan):
     MACAddr = []
     Vendors = []
     nm = nmap.PortScanner(nmap_search_path = nmap_path)
-    if fullScan == True:        
-        #print("IoT Scan Started")        
+    if fullScan == True:              
         for ip in range(256):
-            #print('192.168.137.'+str(ip))
             scan_result = nm.scan(hosts='192.168.137.'+str(ip), arguments='-sn --max-retries 2')
-            #print(scan_result)
             result = scan_result["scan"]
-            #print(result)
             for i in result.keys():     
                 try:
                     if result[i]["addresses"]["mac"][0:8] in setup["ScanningSettings"]["OUIs"].keys():
-                        #print(result[i]["addresses"]["mac"])
-                        IPAddr.append(result[i]["addresses"]["ipv4"])
+                                            IPAddr.append(result[i]["addresses"]["ipv4"])
                         MACAddr.append(result[i]["addresses"]["mac"])
-                        Vendors.append(result[i]["vendor"][result[i]["addresses"]["mac"]])
-                        #print(result[i]["vendor"][result[i]["addresses"]["mac"]])
+                        Vendors.append(result[i]["vendor"][result[i]["addresses"]["mac"]])                        
                 except:
                     pass
         return IPAddr, MACAddr, Vendors
     elif fullScan == False:        
         IPList = setup["ScanningSettings"]["DeviceIPs"]        
-        for ip in IPList:
-            #print('192.168.137.'+str(ip))
-            scan_result = nm.scan(hosts=ip, arguments='-sn --max-retries 2')
-            #print(scan_result)
-            result = scan_result["scan"]
-            #print(setup["ScanningSettings"]["OUIs"].keys())
-            #print(result)
+        for ip in IPList:            
+            scan_result = nm.scan(hosts=ip, arguments='-sn')
+            print(scan_result)
+            result = scan_result["scan"]            
             for i in result.keys():     
                 try:
-                    if result[i]["addresses"]["mac"][0:8] in setup["ScanningSettings"]["OUIs"].keys():
-                        #print(result[i]["addresses"]["mac"])
+                    if result[i]["addresses"]["mac"][0:8] in setup["ScanningSettings"]["OUIs"].keys():                        
                         IPAddr.append(result[i]["addresses"]["ipv4"])
                         MACAddr.append(result[i]["addresses"]["mac"])
-                        Vendors.append(result[i]["vendor"][result[i]["addresses"]["mac"]])
-                        #print(result[i]["vendor"][result[i]["addresses"]["mac"]])
-                except Exception as e:
-                    #print(e)
+                        Vendors.append(result[i]["vendor"][result[i]["addresses"]["mac"]])                        
+                except Exception as e:                  
                     pass
         return IPAddr, MACAddr, Vendors
 
+"""
+Used to generate the data source in grafana using the grafana API
+After creating the data source it quries it
+This is done as a data source can only be used once it's been verified, which can be done by querying it from Grafana
+"""
 def datasourceGenerator(dbname, dbip, dbport, password, user, grafanaApi, grafanaip, grafanaport):
     query = {
   "from": "1420066800000",
@@ -121,31 +117,54 @@ def datasourceGenerator(dbname, dbip, dbport, password, user, grafanaApi, grafan
             "sslmode": "disable"
           }
         }
-    #creates a new data source, if it already exists and error is thrown
+    
     try:
         r = grafanaApi.datasource.create_datasource(DatasourceJson)
         r = grafanaApi.datasource.get_datasource_by_name(dbname)               
         query["queries"][0]["datasourceId"] = r["id"]
         response = requests.post("http://{}:{}/api/tsdb/query".format(grafanaip, grafanaport), data = query)  
     except Exception as e:
-        #print(e)
+        
         pass
+"""
+Generates dashboards based off of the Dashboard.json included in the same directory
+changes the json so that the queries now pull data for the device the dashboard is associated to
+Also tags the dashboard as to make it easier to identify what the IP relates to  
+
+"""
 
 def dashboardGenerator(mac, ip, vendor, grafanaApi):    
     f = open("Dashboard.json")
     panels = json.load(f)
-    panelStr = json.dumps(panels)    
-    #Update Base Dashboard for the Specific Device
-    panelStr = panelStr.replace('60:1d:9d:dd:36:aa', mac.lower())
-    panelStr = panelStr.replace('192.168.137.199', ip)    
-    panels = json.loads(panelStr)
-    myDashboard = {'id': None, 'uid': None, 'title': ip, 'tags': [vendor],"time": {
-    "from": "now-5m",
-    "to": "now"
-  }, 'timezone': 'browser', "panels": panels,'schemaVersion': 0, 'version': 0, "graphTooltip": 1, "refresh":"5s"}
-    r = grafanaApi.dashboard.update_dashboard({'dashboard': myDashboard, 'overwrite': True, 'isStarred': False})
+    for i in range(len(mac)):
+        panelStr = json.dumps(panels)    
+
+        panelStr = panelStr.replace('dc:a6:32:98:ef:c7', mac[i].lower())
+        panelStr = panelStr.replace('192.168.137.196', ip[i])    
+        panels = json.loads(panelStr)
+        myDashboard = {'id': None, 'uid': None, 'title': ip[i], 'tags': [vendor[i]],"time": {
+        "from": "now-5m",
+        "to": "now"
+      }, 'timezone': 'browser', "panels": panels,'schemaVersion': 0, 'version': 0, "graphTooltip": 1, "refresh":"5s"}
+        r = grafanaApi.dashboard.update_dashboard({'dashboard': myDashboard, 'overwrite': True, 'isStarred': True})
+
+"""
+Starts the sniffer on the configured interface
+applys a filter so that it only collects packets relating to hosts that were found during the scan
+"""
+def sniffer():
+    sniff(session=NetflowSession, iface = r"\Device\NPF_{}".format(setup["NetworkInterface"]), filter=(hostFilter), prn=packetProcessing)
     
-    
+
+"""
+Main, used primarily to call all the different functions
+Loads config file srettings in
+sets up the initial grafana api client
+Loads in the model and scaler
+sets up the connection between the script and the database
+generates the host filter to be used when sniffing
+
+"""
 if __name__ == "__main__":
     f = open("Config.json")
     setup = json.load(f)
@@ -166,24 +185,17 @@ if __name__ == "__main__":
         try:
             grafanaApi.dashboard.delete_dashboard(i["uid"])
         except:
-            pass
-    for i in range(len(IPAddr)):
-        dashboardGenerator(MACAddr[i], IPAddr[i], Vendors[i], grafanaApi)
+            pass    
+    dashboardGenerator(MACAddr, IPAddr, Vendors, grafanaApi)
     Model = joblib.load("Model.pkl")
     Scaler = joblib.load("Scaler.pkl")
     try:
         conn = psycopg2.connect("dbname='{}' user='{}' host=\'{}\' password='{}'".format(dbname, user, dbip, password))
-        #print("Connection Successful")
     except Exception as e:
         print(e)
         quit()
     
-        #print(e)
-        #print("I am unable to connect to the database")
-        #print(e)
-    #time.sleep(60)
-    #CamAddress = list(Links.keys())
-    #print("host {}".format(CameraAddr[0]))
+        
     if len(IPAddr) < 1:
         print("No devices have been found")
         quit()
@@ -194,5 +206,6 @@ if __name__ == "__main__":
             for ip in IPAddr:
                 hostFilter = hostFilter + " or {}".format(ip)
     print(hostFilter)
-    sniff(session=NetflowSession, iface = r"\Device\NPF_{}".format(setup["NetworkInterface"]), filter=(hostFilter), prn=test)
+    sniffer()
+    
     
